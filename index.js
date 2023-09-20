3d852b5905ca035983e4a4ecb9aa5dfe2d8f0fb9
shawnmcclure <shawn850mcclure@gmail.com>
2023-09-19 22:44:44 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1*num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8
+let num4 = 2
+const mod = num3/num4
+
+const max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
